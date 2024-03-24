4a8e80bd095a4600303eed15b94b6f494f4b317a
adyfp24 <adyfp24@gmail.com>
2024-03-24 18:32:56 +0700

    feat: adding base code for websocket

--- server.js
+++ server.js
@@ -1,16 +1,51 @@
-const express = require('express');
-const cors= require('cors');
-const app = express();
-const PORT = 4000;
-const pengepulRoute = require('./routes/pengepulRoute');
-const authRoute = require('./routes/authRoute');
+//npm install websocket
+var WebSocketServer = require('websocket').server;
+var http = require('http');
 
-app.use(cors());
-app.use(express.json());
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(5000, function() {
+    console.log((new Date()) + ' Server is listening on port 5000');
+});
 
-app.use('/api/v1/', authRoute);
-app.use('/api/v1/', pengepulRoute);
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
 
-app.listen(PORT, ()=> {
-    console.log(`server running on port ${PORT}`);
-}); 
+function originIsAllowed(origin) {
+  return true;
+}
+
+wsServer.on('request', function(request) {
+    console.log(request)
+    if (!originIsAllowed(request.origin)) {
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept(null, request.origin)
+    console.log((new Date()) + ' Connection accepted.');
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            //connection.sendUTF(message.utf8Data); this resend the reseived message, instead of it i will send a custom message. hello from nodejs
+            connection.sendUTF("Hello from node.js");
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+
+
+
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
