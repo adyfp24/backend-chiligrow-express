9df232236838b6ee9a98de1dc40be75468fa9fd1
adyfp24 <adyfp24@gmail.com>
2024-03-27 15:49:02 +0700

    feat: adding controller and service for sensor's handling

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ function originIsAllowed(origin) {
   return true;
 }
 
+const sensorController = require('./controllers/penyiramanController');
+
 wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection request from origin ' + request.origin);
 
@@ -25,16 +27,7 @@ wsServer.on('request', function(request) {
     const connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
 
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Data sensor kelembapan tanah: ' + message.utf8Data);
-            connection.sendUTF("Hello from Node.js");
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
+    connection.on('message', sensorController.sensorHandler);
 
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
