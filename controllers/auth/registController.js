0a247a9e29e0b2b5d633185bb11637580cd7efb3
adyfp24 <adyfp24@gmail.com>
2024-04-08 11:11:40 +0700

    fix: fix expired time for jwt token

--- controllers/auth/registController.js
+++ controllers/auth/registController.js
@@ -13,7 +13,7 @@ const register = async (req, res) => {
 
         const newUser = await authService.registerService(userData);
 
-        return res.status(200).json({
+        return res.status(201).json({
             success: true,
             message: 'registrasi sukses',
             data: newUser
--- utils/signToken.js
+++ utils/signToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateJWT = (id_user) => {
-    const apiToken = jwt.sign({ id: id_user }, process.env.JWT_SECRET, { expiresIn: '30m' });
+    const apiToken = jwt.sign({ id: id_user }, process.env.JWT_SECRET);
     return apiToken;
 }
 
