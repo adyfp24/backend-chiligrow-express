22fb45cf1916d23cd61373ac3c88ea982de259d0
adyfp24 <adyfp24@gmail.com>
2024-05-30 11:41:56 +0700

    fix: minor bug

--- services/simulasiService.js
+++ services/simulasiService.js
@@ -30,17 +30,17 @@ const getResult = (luas_lahan, jenis_bibit) => {
     let jumlah_bibit, kuantitas_pupuk, pupuk_urea, pupuk_npk, volume_air;
 
     if (jenis_bibit == "rawit") {
-        jumlah_bibit = luas_lahan / 4;
-        kuantitas_pupuk = jumlah_bibit * 8;
-        pupuk_urea = kuantitas_pupuk / 1;
-        pupuk_npk = kuantitas_pupuk / 2;
-        volume_air = jumlah_bibit * 8;
-    } else if (jenis_bibit == "merah") {
-        jumlah_bibit = luas_lahan / 3;
-        kuantitas_pupuk = jumlah_bibit * 10;
+        jumlah_bibit = luas_lahan / 0.3;
+        kuantitas_pupuk = jumlah_bibit * 160;
         pupuk_urea = kuantitas_pupuk / 2;
-        pupuk_npk = kuantitas_pupuk / 1;
-        volume_air = jumlah_bibit * 10;
+        pupuk_npk = kuantitas_pupuk / 2;
+        volume_air = jumlah_bibit * 100;
+    } else if (jenis_bibit == "merah") {
+        jumlah_bibit = luas_lahan / 0.2;
+        kuantitas_pupuk = jumlah_bibit * 160;
+        pupuk_urea = kuantitas_pupuk / 2;
+        pupuk_npk = kuantitas_pupuk / 2;
+        volume_air = jumlah_bibit * 100;
     };
 
     const dataSimulasi = {
